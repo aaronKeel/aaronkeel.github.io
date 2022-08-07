73c42846eb7e4203b1525e8758dd1f96d31d6130
aaronKeel <18557363+aaronKeel@users.noreply.github.com>
2022-08-06 22:19:54 -0600

    update scripts

---
+++ app.11d2415f356c939db59d.bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('script loaded');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLjExZDI0MTVmMzU2YzkzOWRiNTlkLmJ1bmRsZS5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYWFyb25rZWVsLmdpdGh1Yi5pby8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnc2NyaXB0IGxvYWRlZCcpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
