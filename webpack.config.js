aa2d7fe44464a925f8bc3ffc3f68635c92f1ab67
aaronKeel <18557363+aaronKeel@users.noreply.github.com>
2022-08-06 22:07:50 -0600

    set up build

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/templates/index.html',
+      favicon: 'src/assets/favicon.png',
+      filename: 'index.html',
+    }),
+  ],
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].[contenthash].bundle.js',
+    chunkFilename: '[name].[contenthash].bundle.js',
+    path: path.resolve(__dirname),
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.js', 'jsx'],
+  },
+  module: {
+    rules: [],
+  },
+};
