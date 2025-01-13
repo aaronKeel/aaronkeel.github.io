4a7d48786d5f9dee25977b6c5e32221fa2cdb5f1
aaronKeel <18557363+aaronKeel@users.noreply.github.com>
2025-01-13 08:54:54 -0700

    update

---
+++ assets/coordinates-BQOJtrT9.js
@@ -0,0 +1,1 @@
+import"./style-DPpCp0Kt.js";import{a,A as e}from"./utils-ir1UDQUZ.js";a("base-axes","Base set of axes.");new e({targetId:"#base-axes",width:800,height:500,xDomain:[-2,6],yDomain:[-1,4]});
--- assets/coordinates-CD2fkj62.js
+++
@@ -1,1 +0,0 @@
-import"./style-DT6vh0K4.js";import{a,A as e}from"./utils-ir1UDQUZ.js";a("base-axes","Base set of axes.");new e({targetId:"#base-axes",width:800,height:500,xDomain:[-2,6],yDomain:[-1,4]});
